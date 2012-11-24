9b86694482c59fa28392b66b25c96cff7f3339d6
Bjorn Borud <bborud@gmail.com>
2012-11-24 11:14:42 +0100

    Added some configuration to HTTP client and some comments.

--- src/main/java/org/cloudname/copkg/Downloader.java
+++ src/main/java/org/cloudname/copkg/Downloader.java
@@ -6,6 +6,7 @@ import com.ning.http.client.HttpResponseBodyPart;
 import com.ning.http.client.HttpResponseHeaders;
 import com.ning.http.client.HttpResponseStatus;
 import com.ning.http.client.ListenableFuture;
+import com.ning.http.client.AsyncHttpClientConfig;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -22,7 +23,19 @@ import java.util.logging.Logger;
  */
 public class Downloader {
     private static final Logger log = Logger.getLogger(Downloader.class.getName());
-    private final AsyncHttpClient client = new AsyncHttpClient();
+
+    private static final String USER_AGENT = "copkg/1";
+
+    // Initial connect timeout a bit low in order to back off faster.
+    private static final int CONNECTION_TIMEOUT_MS = (15 * 1000);
+
+    // When we are connected and things are slow we might as well wait.
+    private static final int REQUEST_TIMEOUT_MS = (5 * 60 * 1000);
+
+    private static final int MAX_RETRY_ON_IOEXCEPTION = 5;
+    private static final int MAX_CONNECTIONS_PER_HOST = 3;
+    private static final int MAX_NUM_REDIRECTS = 3;
+    private final AsyncHttpClient client;
 
     /**
      * Result of download.
@@ -53,6 +66,24 @@ public class Downloader {
     }
 
     /**
+     * Configure and construct the downloader.
+     */
+    public Downloader() {
+        AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder()
+            .setCompressionEnabled(true)
+            .setConnectionTimeoutInMs(CONNECTION_TIMEOUT_MS)
+            .setRequestTimeoutInMs(REQUEST_TIMEOUT_MS)
+            .setFollowRedirects(true)
+            .setMaximumNumberOfRedirects(MAX_NUM_REDIRECTS)
+            .setAllowPoolingConnection(true)
+            .setMaxRequestRetry(MAX_RETRY_ON_IOEXCEPTION)
+            .setMaximumConnectionsPerHost(MAX_CONNECTIONS_PER_HOST)
+            .setUserAgent(USER_AGENT)
+            .build();
+        client = new AsyncHttpClient(config);
+    }
+
+    /**
      * Download a file into a specified destination.
      *
      * @param url the URL we wish to download.
@@ -74,6 +105,7 @@ public class Downloader {
                     // Abort the download if we had an error writing
                     // the body part to file since it is unlikely we
                     // can continue.
+                    new File(destination).delete();
                     return AsyncHandler.STATE.ABORT;
                 }
                 return AsyncHandler.STATE.CONTINUE;
@@ -84,7 +116,13 @@ public class Downloader {
                 try {
                     outputStream.close();
                 } catch (IOException e) {
-                    // There isn't really a lot we can do at this point.
+                    // There isn't really a lot we can do at this
+                    // point.  Might as well just swallow the
+                    // exception and hope the file is okay.  If it
+                    // isn't okay that is something that has to be
+                    // determined during the verification phase.
+                    // Deleting the file might not be what the user
+                    // wants so we leave it alone.
                     log.log(Level.WARNING, "Got IOException", e);
                 }
                 return new File(destination);
@@ -97,6 +135,10 @@ public class Downloader {
 
             @Override
             public AsyncHandler.STATE onStatusReceived(HttpResponseStatus responseStatus) {
+                // TODO(borud): need to test what happens when we get
+                //   a redirect.  The below code might actually abort
+                //   following a redirect and that is not what we
+                //   want.
                 if (responseStatus.getStatusCode() != 200) {
                     return AsyncHandler.STATE.ABORT;
                 }
